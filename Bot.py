4f0db71831f77b578a8f23c261ccee0d3b2584a8
ngna8947-sys <ngna8947@gmail.com>
2026-09-17 17:22:43 +0700

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -53,14 +53,14 @@ BOT_TOKEN = "8914728102:AAFCUOmvtYKp3LLoBlg4H4Fbz5PE8joN2zU"
 ADMIN_ID = 5915683588
 
 BAKONG_TOKEN = "rbkMVUSQPooaey51jm1cD5ECnzmHyeNX7fBX4Afc16GU8k"
-BANK_ACCOUNT = "samnang_mon@bkrt"
+BANK_ACCOUNT = "mon_samnang@bkrt"
 MERCHANT_NAME = "KhmerSMM"
 MERCHANT_CITY = "Phnom Penh"
 DEPOSIT_EXPIRE_SEC = 300
 POLL_INTERVAL = 5
 
-# 💎 កូដ QR String ថេរ (Static KHQR) ត្រឹមត្រូវស្ដង់ដារបាគងរបស់អ្នក
-MY_STATIC_QR = "00020101021129190011kh.gov.nbc.bakong0115samnang_mon@bkrt5204599953038405802KH5909KhmerSMM6010Phnom Penh6304"
+# 💎 កូដ QR String ថេរ (Static KHQR) ត្រូវនឹង mon_samnang@bkrt
+MY_STATIC_QR = "00020101021129190011kh.gov.nbc.bakong0115mon_samnang@bkrt5204599953038405802KH5909KhmerSMM6010Phnom Penh6304"
 
 WALLETS_FILE = "smm_wallets.json"
 USERS_FILE = "smm_users.json"
